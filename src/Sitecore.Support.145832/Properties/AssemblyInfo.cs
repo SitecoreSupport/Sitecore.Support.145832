1d215794d532bdafb46a325afa624e3ea6093975
DK\MVO <mvo@sitecore.net>
2017-02-03 12:35:29 +0200

    fix the issue related to upload media files with the same names

---
+++ src/Sitecore.Support.145832/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.145832")]
+[assembly: AssemblyProduct("Sitecore.Support.145832")]
+[assembly: ComVisible(false)]
